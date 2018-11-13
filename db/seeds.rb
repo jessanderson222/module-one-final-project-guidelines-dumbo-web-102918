7f685df4d73d135142885994187a2a8b6e6729c4
jessanderson222 <jessanderson222@gmail.com>
2018-11-13 14:41:59 -0500

    recipe puppy API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-#require 'rapidapisdk'
-#RapidAPI.config(project:'default-application_5be9f912e4b09efa5fbd0aa4',token: '2188b3b3-a5db-4771-a203-64f9d9771151');
-
-#data_string = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/479101/information",
-#  headers:{
-#    "X-Mashape-Key" => "jFchjAsPXKmsh2i69Baw5gwgXBeMp1Qz7ibjsnCGnVZDXPtYx8",
-#    "X-Mashape-Host" => "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-#  })
-#response = rapid.call('NasaAPI', 'getPictureOfTheDay', {});
+recipe_string = RestClient.get('http://www.recipepuppy.com/api')
+recipe_hash = JSON.parse(recipe_string)
+binding.pry
