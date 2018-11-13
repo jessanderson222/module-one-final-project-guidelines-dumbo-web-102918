a6ff0fb30d5ba9d17b2cd43b8ed044610a741ee7
jessanderson222 <jessanderson222@gmail.com>
2018-11-13 15:03:27 -0500

    returning recipe_name and ingredient_recipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
 recipe_string = RestClient.get('http://www.recipepuppy.com/api')
 recipe_hash = JSON.parse(recipe_string)
+
+puts recipe_hash["results"][0]
+
+recipe_name = recipe_hash["results"].map do |recipe|
+  recipe["title"]
+end
+
+ingredient_recipe = recipe_hash["results"].map do |recipe|
+  recipe["ingredients"]
+end
+
+
+
 binding.pry
